a4b8dbf412c4d4da74a6ab61d3bf561d372e1a27
Ella Gading <ellagading@ellas-mbp.myfiosgateway.com>
2022-01-27 19:40:45 -0500

    changed the format

--- easy/question.js
+++ easy/question.js
@@ -3,24 +3,24 @@
 
 // Functions 
 function buildQuestion(){}
-function showAnswers(){}
+function showAnswer(){}
 
 // Variables
 const questionContainer = document.getElementById('question');
-const answersContainer = document.getElementById('answers');
+const answerContainer = document.getElementById('answer');
 const submitButton = document.getElementById('submit');
 
 // Display question
 buildQuestion();
 
 // Event Listeners. When submit is click, show answers
-submitButton.addEventListener('click', showAnswers);
+submitButton.addEventListener('click', showAnswer);
 
 
 // Using an array will make the questions to iterate over.
-const myQuestions = [
+const myQuestion = [
     {
-        question: "If 1=3, 2=3, 3=5, 4=4, 5=4, Then, 6=?" ,
+        question: "If 1 = 3, 2 = 3, 3 = 5, 4 = 4, 5 = 4, Then, 6 = ?" ,
         answers: {
             a: "4",
             b: "3",
@@ -28,51 +28,35 @@ const myQuestions = [
         },
         correctAnswer: "is 3, because 'six' has three letters" 
     },
-    {
-        question: "Using only an addition, how do you add eight 8's and get the number 1000?" , 
-        answers: {
-            a: "88 + 888", 
-            b: "88888 + 88",
-            c: "888 + 88 + 8 + 8 + 8",
-        },
-        correctAnswer: "is c, because 888 + 88 + 8 + 8 + 8= 1000"
-    },
-    {
-        question: "Look at this series: 36, 34, 30, 28, 24, _. What number should come next?" ,
-        answers: {
-            a: "22",
-            b: "21",
-            c: "20",
-        },
-        correctAnswer: "is a: 22"
-    }
+
+
 ]; // Note: these questions will appear in the order they're listed, as this is an array.
 
-// Calling out functions.
+// Calling out function.
 function buildQuestion(){}
     // Variable to store the HTML output 
     const output = [];
 
 // For each question
-myQuestions.forEach(
+myQuestion.forEach(
     (currentQuestion, questionNumber) => {
         // Variable to store the list of possible answers
         const answers = [];
         // For each available answer
-        for(letter in currentQuestion.answers){
+        for(letter in currentQuestion.answer){
             // add an HTML radio button
-            answers.push(
+            answer.push(
                 `<label>
                 <input type="radio" name="question${questionNumber}" value=${letter}">
                 ${letter} :
-                ${currentQuestion.answers[letter]}
+                ${currentQuestion.answer[letter]}
                 </label>` 
             );
             }
-        // Question and Answers will be added to the output
+        // Question and Answer will be added to the output
         output.push(
             `<div class="question"> ${currentQuestion.question} </div>
-            <div class="answers"> ${answers.join('')} </div>`
+            <div class="answer"> ${answer.join('')} </div>`
         );
     }
 );
@@ -82,34 +66,34 @@ myQuestions.forEach(
 // Show Answers Function
 function showAnswers(){
     // Containers from our question
-    const answersContainer = questionContainer.querySelectorAll('.answers');
+    const answerContainer = questionContainer.querySelectorAll('.answer');
 
     // The list of user's answers
     let numCorrect = 0;
 
     // For each question
-    myQuestions.forEach((currentQuestion, questionNumber) => {
+    myQuestion.forEach((currentQuestion, questionNumber) => {
         
         // Selected answer
-        const answersContainer = answersContainers[questionNumber];
+        const answerContainer = answerContainers[questionNumber];
         const selector = `input[name=question${questionNumber}]:checked`
-        const userAnswer = (answersContainer.querySelector(selector) || {}).value; // ||: prevents from crashing the game. If the user leaves it blank or skips the question, it will be undefined or the player's answer.
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value; // ||: prevents from crashing the game. If the user leaves it blank or skips the question, it will be undefined or the player's answer.
 
         // If answered correct
         if(userAnswer === currentQuestion.correctAnswer){
             // Add to the number of correct answers
             numCorrect++;
             // Correct answers is in green
-            answersContainers[questionNumber].style.color = 'lightgreen';
+            answerContainers[questionNumber].style.color = 'lightgreen';
         }
             // If answer is wrong or blank
             else{
                 // Color the answers red
-                answersContainers[questionNumber].style.color = 'red';
+                answerContainers[questionNumber].style.color = 'red';
             }
         });
         // Show number of correct answers 
-        answersContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}` ;
+        answerContainer.innerHTML = `${numCorrect} out of ${myQuestion.length}` ;
 }
 
 })();
